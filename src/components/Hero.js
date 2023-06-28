58deebadf9d0af7251944dfc3fb798c1c50b6260
Vitaly Vorobyev <vitaly.vorobyev@cern.ch>
2023-06-28 07:09:51 +0200

    css hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,16 @@
+import Photo from "../images/restauranfood.jpg"
+
 const Hero = () => {
     return (
         <section class="hero">
             <div class="hero-card">
                 <h1 class="hero-title">Little Lemon</h1>
                 <h2 class="hero-subtitle">Chicago</h2>
-                <div class="hero-text"></div>
+                <div class="hero-text">We are family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist</div>
+                <button class="hero-button">Reserve a table</button>
             </div>
             <div class="hero-img-container">
-                <img class="hero-img" src="" alt="A restaurant dish"/>
+                <img class="hero-img" src={Photo} alt="A restaurant dish"/>
             </div>
         </section>
     )
