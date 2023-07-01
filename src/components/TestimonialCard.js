b6b580cca8293f84ccfb882fc6a7cbecb19defd0
Vitaly Vorobyev <vitaly.vorobyev@cern.ch>
2023-07-01 14:16:54 +0200

    css html testimonials almost done

--- src/components/TestimonialCard.js
+++ src/components/TestimonialCard.js
@@ -1,9 +1,14 @@
 
-const TestimonialCard = () => {
+const TestimonialCard = (props) => {
     return (
-        <section class="testimonial-card">
-
-        </section>
+        <div class="testimonial-card">
+            <div class='testimonial-rating'>{props.rating}</div>
+            <div class='testimonial-img-container'>
+                <img class='testimonial-img' src={props.img} alt='person avatar'/>
+            </div>
+            <div class='testimonial-name'>{props.name}</div>
+            <div class='testimonial-text'>{props.text}</div>
+        </div>
     )
 };
 
--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,8 +1,40 @@
+import TestimonialCard from "./TestimonialCard";
+
+import Person1Img from "../images/img3.png"
+import Person2Img from "../images/img4.png"
+import Person3Img from "../images/img7.png"
+import Person4Img from "../images/img6.png"
 
 const Testimonials = () => {
     return (
         <section class="testimonials">
-            <h1 class="section-header">Testimonials</h1>
+            <h1 class="testimonials-header">Testimonials</h1>
+            <div class='testimonials-reviews'>
+                <TestimonialCard
+                    rating='4.5'
+                    img={Person1Img}
+                    name='Nick Norton'
+                    text='Review text'
+                />
+                <TestimonialCard
+                    rating='4.0'
+                    img={Person2Img}
+                    name='Ella Fitzgerald'
+                    text='Review text'
+                />
+                <TestimonialCard
+                    rating='5.0'
+                    img={Person3Img}
+                    name='Hannah Arendt'
+                    text='Review text'
+                />
+                <TestimonialCard
+                    rating='4.5'
+                    img={Person4Img}
+                    name='Alec Baldwin'
+                    text='Review text'
+                />
+            </div>
         </section>
     )
 };
